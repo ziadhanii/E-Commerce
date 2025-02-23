b5f6dd6e4143a78dcd7707f38dc949b64e4e3a5d
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-02-23 05:04:38 +0200

    Add auth and blank layout components with initial templates and styles; update main color in styles.scss

---
+++ src/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.scss'
+})
+export class AuthLayoutComponent {
+
+}
---
+++ src/layouts/blank-layout/blank-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blank-layout',
+  imports: [],
+  templateUrl: './blank-layout.component.html',
+  styleUrl: './blank-layout.component.scss'
+})
+export class BlankLayoutComponent {
+
+}
