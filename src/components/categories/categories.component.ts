fc7e631eec28fe22b3da6b368121323d72dd48e1
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-03-01 14:58:43 +0200

    Implement categories component with dynamic category retrieval and display

--- src/components/categories/categories.component.ts
+++ src/components/categories/categories.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../core/services/product.service';
+import { Category } from '../../core/interfaces/category';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
+  constructor(private productService: ProductService) { }
+  ngOnInit(): void {
+    this.productService.getCategories().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.categories = response.data;
+      }
+    });
+
+  }
 
 }
